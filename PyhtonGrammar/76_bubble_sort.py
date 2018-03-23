8cc7744516b5da984b9c20784f4bb4c7e4ba473e
huangcaiyan520 <374342187@qq.com>
2018-03-23 18:36:21 +0800

    冒泡排序、快速排序

--- PyhtonGrammar/76_bubble_sort.py
+++ PyhtonGrammar/76_bubble_sort.py
@@ -3,19 +3,39 @@
 # @File : 76_bubble_sort
 # @Software : PyCharm
 
-def bubble_sort( lists ):
-    # 获取列表长度
+# def bubble_sort( lists ):
+#     # 获取列表长度
+#     count = len( lists ) - 1
+#     # n 个元素遍历 n 次
+#     for index in range( count , 0 , -1 ):
+#         # 第 i 个和第 i＋1 个进行对比
+#         # print(index)
+#         print( 'yayaya:' , lists )
+#         for sub_index in range( index ):
+#             print( sub_index )
+#             if lists[ sub_index ] > lists[ sub_index + 1 ]:
+#                 lists[ sub_index ] , lists[ sub_index + 1 ] = lists[ sub_index + 1 ] , lists[ sub_index ]
+#                 print( '呵呵哒：' , lists )
+#
+#     return lists
+#
+#
+# # alist = [ 0 , 10 , 88 , 19 , 9 , 1 ]
+# alist = [ 'A' , 'Z' , 'K' , 'C' , 'F' , 'B' ]
+#
+# print( bubble_sort( alist ) )
+
+
+def bubble_sorts( lists ):
     count = len( lists ) - 1
-    # n 个元素遍历 n 次
-    for index in range( count , 0 , -1 ):
-        # 第 i 个和第 i＋1 个进行对比
-        print(index)
-        for sub_index in range( index ):
-            print( sub_index )
-            if lists[ sub_index ] > lists[ sub_index + 1 ]:
-                lists[ sub_index ] , lists[ sub_index + 1 ] = lists[ sub_index + 1 ] , lists[ sub_index ]
+    for i in range( 0 , count ):
+        for j in range( i + 1 , count ):
+            if lists[ i ] > lists[ j ]:
+                lists[ i ] , lists[ j ] = lists[ j ] , lists[ i ]
+                print( 'lists=' , lists )
+    
     return lists
 
 
 alist = [ 0 , 10 , 88 , 19 , 9 , 1 ]
-print( bubble_sort( alist ) )
+print( 'bubble_sorts==' , bubble_sorts( alist ) )
